5cd27b64cd0c59498b73c5d844f4b11c749fcc78
Ajishi2 <167234249+Ajishi2@users.noreply.github.com>
2025-01-19 18:48:00 +0530

    Update Ap.js

--- frontend/src/Ap.js
+++ frontend/src/Ap.js
@@ -1,6 +1,7 @@
+import FollowingList from './components/FollowingList'; // Import the component
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import UserSearch from './components/UserSearch';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import routing components
+import UserSearch from './components/UserSearch'; // Correct path to your components
 import RepoList from './components/RepoList';
 import FollowerList from './components/FollowerList';
 import RepoDetails from './components/RepoDetails';
@@ -13,7 +14,8 @@ const Ap = () => {
         <Routes>
           <Route path="/" element={<UserSearch />} />
           <Route path="/repos/:username" element={<RepoList />} />
-          <Route path="/followers/:username" element={<FollowerList />} /> {/* Followers Route */}
+          <Route path="/followers/:username" element={<FollowerList />} />
+          <Route path="/following/:username" element={<FollowingList />} /> {/* Following Route */}
           <Route path="/repo-details/:username/:repoName" element={<RepoDetails />} />
         </Routes>
       </div>
