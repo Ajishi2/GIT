95011feb90f5cb338a549144f080838ecbbef050
Ajishi2 <167234249+Ajishi2@users.noreply.github.com>
2025-01-19 19:20:04 +0530

    Update database.ts

--- Backend/src/database.ts
+++ Backend/src/database.ts
@@ -1,34 +1,32 @@
 import { DataSource } from 'typeorm';
-import { User } from './entity/User'; // Adjust the import based on your file structure
+import { User } from './entity/User';
 import dotenv from 'dotenv';
 
-// Load environment variables from .env file
+
 dotenv.config();
 
-// Create a DataSource instance
+
 export const AppDataSource = new DataSource({
-  type: process.env.DB_TYPE as 'mysql',  // Ensure type is correct
+  type: process.env.DB_TYPE as 'mysql', 
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT as string, 10),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: [User ], // Specify your entities here
-  logging: false, // Set to true for logging SQL queries
-  synchronize: true, // Auto-create tables in development
+  entities: [User ], 
+  logging: false, 
+  synchronize: true, 
 });
 
-// Function to connect to the database
 export const connectDatabase = async () => {
   try {
     await AppDataSource.initialize();
     console.log('Database connection established successfully.');
   } catch (error: unknown) {
     if (error instanceof Error) {
-      // Now TypeScript knows that 'error' is an instance of Error
+    
       console.error('Database connection error:', error.message);
     } else {
-      // In case the error is not an instance of Error, you can log it as is
       console.error('Unknown error occurred:', error);
     }
   }
